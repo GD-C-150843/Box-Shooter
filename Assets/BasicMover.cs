4621a9b758e60a3f58499ac9d81be22db445dffe
GD-C-150843 <36012742+GD-C-150843@users.noreply.github.com>
2018-04-08 23:48:24 +0500

    Box-Shooter
    Synergy

---
+++ Assets/BasicMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour {
+    public bool spin = true;
+    public float spinSpeed = 180f;
+    public char spinAroundAxis;
+   
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (spin)
+        {
+            if (spinAroundAxis == 'X')
+            {
+                gameObject.transform.Rotate(Vector3.right * spinSpeed * Time.deltaTime);
+            }
+            else if (spinAroundAxis == 'Y')
+            {
+                gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+            }
+            else if (spinAroundAxis == 'Z')
+            {
+                gameObject.transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+
+        }
+        
+	}
+}
---
+++ Assets/Task2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task2 : MonoBehaviour {
+
+    public bool spin = true;
+    public float spinSpeed = 210f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.Rotate(Vector3.left * spinSpeed * Time.deltaTime);
+	}
+}
